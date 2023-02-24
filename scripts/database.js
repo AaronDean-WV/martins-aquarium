03d38fe3f221552e27f9373bcf7b03c938dda28e
Aaron Dean <adean30462@gmail.com>
2023-02-24 10:05:36 -0500

    martins-aquarium

---
+++ scripts/database.js
@@ -0,0 +1,37 @@
+// const database = {
+//     fish: [
+//         {
+//             name: "Bart",
+//             diet: "crustaceans",
+//             size: 3
+//             species:"",
+//             origin:"", 
+        //};
+//         
+
+    //       {  name: "Bubble Bass",
+    //          diet: "Hamburgers with pickles",
+    //          size: 11
+    //          species:"Seabass",
+    //          origin:"Bikini Bottom",
+    //         };
+              
+    //          {  name: "Gary",
+    //             diet: "otherfishes",
+    //             size: 250
+    //             species:"Goliath Grouper",
+    //             origin:"Atlantic Ocean",
+    //         };
+    //         
+    //              {  name: "Tom",
+    //             diet: "shellfish",
+    //             size: 12
+    //             species:"Blobfish",
+    //             origin:"New Zealand",
+    //         };
+//           ]
+
+// export const getFish = () => {
+//     return database.fish.map(copyOfSingleFishObject => ({...copyOfSingleFishObject}))
+// };
+
